a3b8e2ebc5da7a06e9af7da5ef83f0e8b806f53b
cccvvv567 <100312269+cccvvv567@users.noreply.github.com>
2022-03-02 12:24:46 +0800

    Add files via upload

---
+++ src/src/bmi.java
@@ -0,0 +1,16 @@
+public class bmi {
+
+
+    public static void main(String[] args) {
+        float h =1.75f ;
+        int w = 70;
+        float i =calc(h,w);
+        System.out.println(i);
+    }
+    static float calc(float h, int w){
+        return w/(h*h);
+
+
+
+    }
+}
