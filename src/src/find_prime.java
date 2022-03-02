f01c7ebd6d3a9b37792c1fd0952361d02e5a0de3
cccvvv567567 <cccvvv567567@gmail.com>
2022-03-02 10:13:05 +0800

    update

--- src/src/find_prime.java
+++ src/src/find_prime.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class find_prime {
+public class find_prime {
+
+
+    public static void main(String[] args) {
+
+
+        int i, n;
+        for (i = 2; i <= 100; i++) {
+            for (n = 2; n < i; n++) {
+                if (i % n == 0)
+                    break;
+            }
+            if (n>=i)
+                System.out.println(n);
+        }
+
+    }
 }
