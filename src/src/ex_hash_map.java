b4c926b72a5ef4fab43b2886b3bf7732ca61891f
Henry Johnson <jo74705@gmail.com>
2022-03-07 16:01:49 +0800

    Add hashmap example

--- src/src/ex_hash_map.java
+++ src/src/ex_hash_map.java
@@ -1,10 +1,26 @@
 import java.util.HashMap; // import the HashMap class
-import java.util.List;
-
+import java.util.ArrayList;
 public class ex_hash_map {
     public static void main(String[] args) {
-        HashMap<String, List<Integer>> map = new HashMap<String, List<Integer>>();
-
+        HashMap<Integer, ArrayList<Integer>> object = new HashMap<>();
+        System.out.println(object.get(2));
+        int[] numList = {2,3,5,7,11};
+        for(int num: numList){
+            for (int i = 1; i <= 100; i++) {
+                if (i % num == 0) {
+                    if(object.get(num) == null){
+                        ArrayList<Integer> arrList = new ArrayList<>();
+                        arrList.add(i);
+                        object.put(num,arrList);
+                    }else {
+                        ArrayList<Integer> arrList = object.get(num);
+                        arrList.add(i);
+                        object.put(num,arrList);
+                    }
+                }
+            }
+        }
+        System.out.println(object);
     }
 }
 
