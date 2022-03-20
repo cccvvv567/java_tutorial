98e79dbedff2ea0a18b8a2acde72da14c548eec5
cccvvv567567 <cccvvv567567@gmail.com>
2022-03-21 03:38:31 +0800

    update

--- src/src/ex_hash_map.java
+++ src/src/ex_hash_map.java
@@ -4,9 +4,26 @@ import java.util.List;
 public class ex_hash_map {
     public static void main(String[] args) {
         HashMap<String, List<Integer>> map = new HashMap<String, List<Integer>>();
-
     }
 }
+
+////1-100
+//2,3,5,7,11
+//"2": [4,6,8,10,12,14]
+//"3": [3,6,9,12]
+
+//import java.util.HashMap; // import the HashMap class
+//
+//public class ex_hash_map {
+//    public static void main(String[] args) {
+//        HashMap<String, String> object = new HashMap<String, String>();
+//        object.put("England","London");
+//        object.put("Taiwan","Taipei");
+//        System.out.println("object>>>" + object);
+//        System.out.println("England>>>" + object.get("England"));
+//        System.out.println("Taiwan>>>" + object.get("Taiwan"));
+//    }
+//}
 
 ////1-100
 //2,3,5,7,11
--- src/src/scanner_bmi.java
+++ src/src/scanner_bmi.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class scanner_bmi {
+public class  scanner_bmi {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
